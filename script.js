b1fd68e4e822a0467e7af723c140336c7a159715
Yuli Gonçalves <162021541+yuligoncalves@users.noreply.github.com>
2024-06-06 20:50:48 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let buttons = document.querySelectorAll('button')
 
+let button = ["+", "-", ".", "/", "="];
+console.log(button.includes("+"));
+
+
 buttons.forEach(function(button){
 
     button.addEventListener('click', function(e) {
         const value = e.target.innerText;
+
+        if (value >= 0 || value === "+" || value === "-" || value === "/" || value === "=" || value === "*" || value === ".")  {
+            console.log("tecla válida")
+        }
         
+        // idetificando se foi digitado um número ou um operador
         if (+value >= 0 || value === ".") {
             console.log(value);
         } else {
